f03b60ebbcbef2939e1b5a37daa5e82d25670e22
vera <lerika250293@mail.ru>
2022-03-04 13:22:44 +0300

    all-ready\

--- main.py
+++ main.py
@@ -4,30 +4,36 @@ import requests
 class YandexUploader:
     token = None
     URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-    header = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
+    header = None
 
-    def __init__(self, token, URL, header):
+    def __init__(self, token):
         self.token = token
-        self.URL = URL
-        self.header = header
+        self.header = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': f'OAuth {token}'
+        }
+        print(self.header)
 
     def upload(self, file_path):
         """Метод загружает файл из file_path на яндекс диск"""
         self.file_path = file_path
+        res = requests.get(url=f'{self.URL}?path={self.file_path}', headers=self.header)
+        json_data = res.json()
+        print(res.status_code, json_data)
 
-        res = requests.get(f'{self.URL}/path={self.file_path}, headers=self.header).json()
+        upload_response = requests.put(url=json_data['href'], data=open(file_path, 'rb'))
 
-        with open(file_path, 'r') as f:
-            try:
-                requests.post(res['href'], files={'file_path': f})
-            except KeyError:
-                print(res)
+        return upload_response
 
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = requests.get(f'{URL}/upload?path={file_path}&overwrite={replace}', headers=header).json()
-    with open('t.txt', 'r', encoding="utf-8") as f_1:
+    path_to_file = 'readme.md'
+    with open('t.txt', 'r', encoding="utf-8") as f:
         token = f.read()
-    uploader = YandexUploader(token)
-    result = uploader.upload(path_to_file)
+        token = token.strip()
+        print(token)
+    uploader = YandexUploader(token=token)
+    result = uploader.upload(file_path=path_to_file)
+    print(result.status_code)
